7bb4572a9e522607ed242e0fcbd937eea870747c
GodXero <shansathish33@gmail.com>
2025-03-25 12:55:41 +0530

    Employee repositiry implimentation done

--- EpicEats_Backend/src/main/java/edu/icet/ecom/util/enumaration/EmployeeRole.java
+++ EpicEats_Backend/src/main/java/edu/icet/ecom/util/enumaration/EmployeeRole.java
@@ -1,5 +1,14 @@
 package edu.icet.ecom.util.enumaration;
 
+import java.util.Arrays;
+
 public enum EmployeeRole {
 	CASHIER, MANAGER, CHEF, WAITER, SUPERVISOR;
+
+	public static EmployeeRole fromName (String name) {
+		return name == null ? null : Arrays.stream(EmployeeRole.values()).
+			filter(employeeRole -> employeeRole.name().equalsIgnoreCase(name)).
+			findFirst().
+			orElse(null);
+	}
 }
