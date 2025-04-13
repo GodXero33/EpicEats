e87d1dd01386a39024292e342c764d6a382c7e19
GodXero <godxero.dev@gmail.com>
2025-04-13 20:23:42 +0530

    front-end started. Loging form is building

--- EpicEats_Frontend/src/app/app.component.ts
+++ EpicEats_Frontend/src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'EpicEats_Frontend';
+  showNavbar: boolean = true;
+
+  constructor (private router: Router) {
+    this.router.events.subscribe(event => {
+      const navHidePaths = ['', '/login', '/signup'];
+
+      if (event instanceof NavigationEnd) this.showNavbar = navHidePaths.includes(event.url);
+    });
+  }
 }
--- EpicEats_Frontend/src/app/app.routes.ts
+++ EpicEats_Frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: LoginComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: 'signup',
+		component: SignupComponent
+	}
+];
