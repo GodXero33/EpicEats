58da89af70b65948a83096bcb51511c3136a275f
GodXero <godxero.dev@gmail.com>
2025-04-16 14:57:41 +0530

    Fix: user name on greet message in home page not showing

--- EpicEats_Frontend/src/app/component/home/home.component.ts
+++ EpicEats_Frontend/src/app/component/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +11,14 @@ export class HomeComponent {
   public username: string = 'sathish';
   public greet: string = '';
 
-  constructor () {
+  constructor (private authService: AuthService) {
     this.updateGreet();
     setInterval(this.updateGreet, 1 * 60 * 1000);
   }
 
   private updateGreet () {
     const hours = new Date().getHours();
-    const username = sessionStorage.getItem('username');
+    const username = this.authService.getUsername();
 
     this.greet = hours <= 12 ? 'Morning' : hours <= 15 ? 'Afternoon' : 'evening';
     this.username = username ? username : '';
