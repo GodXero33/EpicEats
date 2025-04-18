8eef6d8472fd9752bdae3a5856515699ce0697ce
GodXero <godxero.dev@gmail.com>
2025-04-18 13:59:47 +0530

    Bug fix: employee repository not deleting promotions histories when employee terminate

--- EpicEats_Backend/src/main/java/edu/icet/ecom/repository/custom/impl/employee/EmployeeRepositoryImpl.java
+++ EpicEats_Backend/src/main/java/edu/icet/ecom/repository/custom/impl/employee/EmployeeRepositoryImpl.java
@@ -209,8 +209,9 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 			return (Integer) this.crudUtil.execute("""
 				UPDATE employee
 				LEFT JOIN employee_shift ON employee.id = employee_shift.employee_id
-				SET employee.is_terminated = TRUE, employee_shift.is_deleted = TRUE
-				WHERE employee.is_terminated = FALSE AND employee.id = ?
+				LEFT JOIN promotion_history ON promotion_history.employee_id = employee.id
+				SET employee.is_terminated = TRUE, employee_shift.is_deleted = TRUE, promotion_history.is_deleted = TRUE
+				WHERE employee.id = ? AND employee.is_terminated = FALSE
 				""", employeeId) == 0 ?
 				new Response<>(null, ResponseType.FAILED) :
 				new Response<>(null, ResponseType.SUCCESS);
