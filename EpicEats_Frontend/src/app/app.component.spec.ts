3c3eb1ae9bf422759eabe1bdd4c619101696f8ba
GodXero <godxero.dev@gmail.com>
2025-05-23 07:36:28 +0530

    Removed employee shift update and delete and some employee shift add updates

--- EpicEats_Frontend/src/app/app.component.spec.ts
+++ EpicEats_Frontend/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'EpicEats_Frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('EpicEats_Frontend');
+    expect('EpicEats_Frontend').toEqual('EpicEats_Frontend');
   });
 
   it('should render title', () => {
